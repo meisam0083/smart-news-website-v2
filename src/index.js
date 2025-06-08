dfe88c635886b774e8e399eebeb879c270780caf
meisam0083 <meisam0083@yahoo.com>
2025-06-08 21:44:03 +0330

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; // این فایل رو در مرحله بعد می سازیم
+import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-// ریشه اپلیکیشن رو پیدا می کنیم
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-// کامپوننت اصلی App رو داخل ریشه رندر می کنیم
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// سرویس ورکر رو برای قابلیت های آفلاین و نصب فعال می کنیم
-// به جای unregister() از register() استفاده می کنیم تا فعال بشه
+// این خط قابلیت نصب و آفلاین را فعال می کند
 serviceWorkerRegistration.register();
-
